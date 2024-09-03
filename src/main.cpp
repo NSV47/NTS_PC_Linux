0945eb692c3ac695bf4566b88117d96e2e26e386
Сергей Новиков <s.novikov@vedapro.ru>
2024-09-03 17:01:05 +0300

    привел в порядок исходный код, удалил лишнее

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 // #define DESTINATION_ADDL 2
 #define DESTINATION_ADDL 3
 
-// HardwareSerial SerialPort(2);
-
 LoRa_E22 e22ttl(18, 17, &Serial1, 8, 46, 3, UART_BPS_RATE_9600, SERIAL_8N1); //  esp32 RX <-- e22 TX, esp32 TX --> e22 RX AUX M0 M1 // UART_BPS_RATE_9600
 
 // put function declarations here:
@@ -17,8 +15,6 @@ void printParameters(struct Configuration configuration);
 void printModuleInformation(struct ModuleInformation moduleInformation);
 void somethingAvailableLORA();
 void initEbyteE22();
-int myFunction(int, int);
-void serialEvent();
 
 bool stringComplete = false;  // whether the string is complete
 String inputString = "";         // a String to hold incoming data
@@ -26,18 +22,14 @@ String inputString = "";         // a String to hold incoming data
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  // SerialPort.begin(115200, SERIAL_8N1, 38, 2);
   initEbyteE22();
   //------------------------------------------
   Serial1.end();
   Serial1.begin(115200, SERIAL_8N1, 18, 17);
-  
-  // SerialPort.println(str);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
   somethingAvailableLORA();
 
   while(Serial.available()>0){
@@ -52,78 +44,21 @@ void loop() {
     ResponseStatus rs = e22ttl.sendFixedMessage(0, DESTINATION_ADDL, 23, inputString); // 20
     inputString = "";
   }
-
-  // if(SerialPort.available()>0){
-    // Serial.write(SerialPort.read());
-  // }
-//--------------------------------------------------
-  if (stringComplete) {
-    // SerialPort.print(inputString);
-  #if 0
-    uint16_t strLen = inputString.length();
-    for(uint16_t i = 0; i < strLen; ++i){
-      if(inputString[i] <= 0xF){
-        SerialPort.print('0');
-      }
-      SerialPort.print(inputString[i], HEX);
-      // SerialPort.print(' ');
-    }
-    SerialPort.println();
-    SerialPort.println();
-
-    inputString = "";
-    stringComplete = false;
-  #endif
-  #if 0
-    SerialPort.println(inputString);
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
-  #endif
-  }
   
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-
-/*
-  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
-  routine is run between each time loop() runs, so using delay inside loop can
-  delay response. Multiple bytes of data may be available.
-*/
-#if 0
-void serialEvent() {
-  if (Serial.available()) { // while
-    char inChar = (char)Serial.read();
-    
-    if(inChar == 0xC0){
-      inputString += 0xC0;
-      stringComplete = true;
-    }else{
-      inputString += inChar;
-    }
-  }
-}
-#endif
 
 void somethingAvailableLORA(){
     // If something available
     if (e22ttl.available()>1) {
         // read the String message
     #ifdef ENABLE_RSSI
-        //ResponseContainer rc = e22ttl.receiveMessageRSSI();
-        ResponseStructContainer rsc = e22ttl.receiveMessageRSSI(sizeof(Message));
+        ResponseContainer rc = e22ttl.receiveMessageRSSI();
     #else
         ResponseContainer rc = e22ttl.receiveMessage();
-        // ResponseStructContainer rsc = e22ttl.receiveMessage(sizeof(Message));
     #endif
         // Is something goes wrong print error
         if (rc.status.code!=1){
             // Serial.println(rc.status.getResponseDescription());
-            // Serial.println(rsc.status.getResponseDescription());
         }else{
             // Print the data received
             // Serial.println(rc.status.getResponseDescription());
@@ -169,11 +104,9 @@ void initEbyteE22(){
     configuration.OPTION.transmissionPower = POWER_24; //POWER_30 // POWER_21
 
     configuration.TRANSMISSION_MODE.enableRSSI = RSSI_DISABLED;                // ENABLE_RSSI
-    //configuration.TRANSMISSION_MODE.enableRSSI = RSSI_ENABLED;                // ENABLE_RSSI
     configuration.TRANSMISSION_MODE.fixedTransmission = FT_FIXED_TRANSMISSION;
     configuration.TRANSMISSION_MODE.enableRepeater = REPEATER_DISABLED;
     configuration.TRANSMISSION_MODE.enableLBT = LBT_DISABLED;
-    //configuration.TRANSMISSION_MODE.WORTransceiverControl = WOR_RECEIVER;
     configuration.TRANSMISSION_MODE.WORTransceiverControl = WOR_TRANSMITTER;
     configuration.TRANSMISSION_MODE.WORPeriod = WOR_2000_011;
 
